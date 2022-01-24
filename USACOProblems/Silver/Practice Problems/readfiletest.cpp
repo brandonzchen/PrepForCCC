23187924a67b8fa39379ecb9e64d56dab8511250
Brandon Chen <brandon.zchen6@gmail.com>
2022-01-24 16:44:57 -0700

    new

--- USACOProblems/Silver/Practice Problems/readfiletest.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	ifstream fin("USACOProblems/Silver/Practice Problems/testfiles.txt");
-	int y, z = 5;
-	int x; fin >> x >> y >> z;
-	cout << x << " " << y << " " << z;
-}
