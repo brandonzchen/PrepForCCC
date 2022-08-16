f155ab52361c255f1e713efbc43e279aa6f696d6
Brandon Chen <brandon.zchen6@gmail.com>
2022-08-16 12:00:13 -0600

    new

---
+++ USACO Silver/3. Graph/2. Flood Fill/Practice/whydidthecowcrosstheroad.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 200;
+int n, cows;
+int grid[MAX_N][MAX_N];
+
+void floodfill(int r, int c) {
+	if (r < 0 || r >= n || c < 0 || c >= n || grid[r][c] >= 2)
+		return;
+	else if (grid[r][c] == 1) {
+		cows++;
+	}
+	grid[r][c] = 3;
+	floodfill(r, c + 1);
+	floodfill(r, c - 1);
+	floodfill(r - 1, c);
+	floodfill(r + 1, c);
+}
+
+int main() {
+	freopen("countcross.in", "r", stdin);
+	freopen("countcross.out", "w", stdout);
+	int k, r;
+	cin >> n >> k >> r;
+	n *= 2;
+	for (int i = 0; i < r; i++) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		x1--; y1--; x2--; y2--;
+		x1 *= 2; y1 *= 2; x2 *= 2; y2 *= 2;
+		if (x1 == x2) {
+			int t = (y1 + y2) / 2;
+			grid[x1][t] = 2;
+			grid[x1 + 1][t] = 2;
+			if (x1 > 0)
+				grid[x1 - 1][t] = 2;
+		}
+		else {
+			int t = (x1 + x2) / 2;
+			grid[t][y1] = 2;
+			grid[t][y1 + 1] = 2;
+			if (y1 > 0)
+				grid[t][y1 - 1] = 2;
+		}
+	}
+	for (int i = 0; i < k; i++) {
+		int x, y;
+		cin >> x >> y;
+		x--; y--;
+		x *= 2; y *= 2;
+		grid[x][y] = 1;
+	}
+	vector<int> c;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (grid[i][j] < 2) {
+				cows = 0;
+				floodfill(i, j);
+				c.push_back(cows);
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < c.size() - 1; i++) {
+		for (int j = i + 1; j < c.size(); j++) {
+			ans += c[i] * c[j];
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
--- USACO Silver/3. Graph/3. Tree Algorithms/Practice/treediametercpp.cpp
+++ USACO Silver/3. Graph/3. Tree Algorithms/Practice/treediametercpp.cpp
@@ -1,23 +1,52 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//void addedge(int x, int y, vector<vector<int>>& adj) {
+//	adj[x].push_back(y);
+//	adj[y].push_back(x);
+//}
+//
+//void bfs()
+//
+//int main() {
+//	int nodes;
+//	cin >> nodes;
+//	vector<vector<int>> tree(nodes + 1, vector<int>());
+//	for (int i = 0; i < nodes - 1; i++) {
+//		int first, second;
+//		cin >> first >> second;
+//		addedge(first, second, tree);
+//	}
+//	cout << "Hello";
+//
+//	
+//}
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void addedge(int x, int y, vector<vector<int>>& adj) {
-	adj[x].push_back(y);
-	adj[y].push_back(x);
-}
+const int mxn = 2e5 + 1;
+int n, dis = 0, end1;
+vector<int> adj[mxn];
 
-void bfs()
+void dfs(int node, int pre, int depth) {
+	for (int i : adj[node])
+		if (i != pre)
+			dfs(i, node, depth + 1);
+	if (depth > dis) dis = depth, end1 = node;
+}
 
 int main() {
-	int nodes;
-	cin >> nodes;
-	vector<vector<int>> tree(nodes + 1, vector<int>());
-	for (int i = 0; i < nodes - 1; i++) {
-		int first, second;
-		cin >> first >> second;
-		addedge(first, second, tree);
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n;
+	for (int i = 0, u, v; i < n - 1; i++) {
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
-	cout << "Hello";
-
-	
+	dfs(1, 0, 0);
+	dfs(end1, 0, 0);
+	cout << dis << "\n";
 }
+
